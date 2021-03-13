966229a776aa3dceceffc84977f8fe6e59609099
kashi-123 <71304061+kashi-123@users.noreply.github.com>
2021-03-13 19:12:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Desk from './Components/Desk';
 import FileUpload from './Components/FileUpload';
 import Personal from './Components/Personal';
 import PersonalDrawer from './Components/PersonalDrawer';
+import Midway from './Components/Midway';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact component={Landing} />
           <Route path="/desk" exact component={Desk} />
           <Route path="/upload" exact component={FileUpload} />
+          <Route path="/mid-way" exact component={Midway} />
           <Route path="/personalupload" exact component={Personal} />
           <Route path="/personaldrawer" exact component={PersonalDrawer} />
 
