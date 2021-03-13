da1854e0b88cc7e445b46d2fbdbc162d3e84c59f
kashi-123 <71304061+kashi-123@users.noreply.github.com>
2021-03-13 13:42:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import './Components/Landing';
 import Landing from './Components/Landing';
 import Desk from './Components/Desk';
+import FileUpload from './Components/FileUpload';
 
 function App() {
   return (
-    <div className="App">
-      <Desk />
-    </div>
+    <Router>
+      <Switch>
+        <div className="App">
+          <Route path="/" exact component={Desk} />
+          <Route path="/upload" exact component={FileUpload} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
